90094554efeec5cd5e9474ffe27d2092694abff0
Dipankar Ganguly <113535570+dipankar2002@users.noreply.github.com>
2022-12-26 11:28:20 +0530

    Update Assign_1.c

--- Assignment 1/Assign_1.c
+++ Assignment 1/Assign_1.c
@@ -4,12 +4,17 @@
 #include <conio.h>
 
 void main() {
+    
     float c,f;
+    clrscr();
+    
     printf("Enter the temperature in Fahrenheit: ");
     scanf("%f",&f);
     
     c=(f-32)*5/9;
 
     printf("The temperature in Celsius is: %.2f",c);
+    
+    getch();
     
 }
